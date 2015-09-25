b67e4ea0ea2b4bf3124cf9951afe898537cb8afa
Joseph McConnell <jjmdevelops@gmail.com>
2015-09-25 14:52:09 -0400

    Got the boat moving and the camera following it

---
+++ Assets/Scripts/boatMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boatMovement : MonoBehaviour {
+
+    float speed = 7.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+
+    }
+}
