4f56815540353ff46ef207c53df77cd805e7b9a9
craigwilliams9992 <craigwilliams@ufl.edu>
2015-09-26 18:22:20 -0400

    bleh

--- Assets/Scripts/cameraMovement.cs
+++ Assets/Scripts/cameraMovement.cs
@@ -11,7 +11,7 @@ public class cameraMovement : MonoBehaviour {
     Vector3 targetPos;
     public float lastVelocity;
     bool decel = false;
-    public float accelScale = 0.0001f;
+    public float accelScale = 5f;
    
     // Use this for initialization
     void Start () {
@@ -29,29 +29,32 @@ public class cameraMovement : MonoBehaviour {
 
             Vector3 targetDirection = (target.transform.position - posNoZ);
 
-           // interpVelocity = targetDirection.magnitude * 5f;
+            interpVelocity = targetDirection.magnitude * 5f;
 
-
-            var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-            if (move.magnitude != 0)
-             {
-
-                    decel = false;
-                    lastVelocity = targetDirection.magnitude * accelScale;
-                    targetPos = transform.position + (targetDirection.normalized * lastVelocity * Time.deltaTime);
-                    if(accelScale<5f)
-                        accelScale *= 1.25f;
-                
-            }
-            else
-            {
-                decel = true;
-                lastVelocity = targetDirection.magnitude * accelScale;
-                targetPos = transform.position + (targetDirection.normalized * lastVelocity * Time.deltaTime);
-                accelScale /= 1.25f;
-            }
+            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
 
             transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+
+            /*
+                        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+                        if (move.magnitude != 0)
+                         {
+
+                                decel = false;
+                                lastVelocity = targetDirection.magnitude * accelScale;
+                                targetPos = transform.position + (targetDirection.normalized * lastVelocity * Time.deltaTime);
+                                if(accelScale < 5f)
+                                    accelScale += 1f;
+
+                        }
+                        else
+                        {
+                            decel = true;
+                            lastVelocity = targetDirection.magnitude * accelScale;
+                            targetPos = transform.position + (targetDirection.normalized * lastVelocity * Time.deltaTime);
+                            accelScale /= 1.09f;
+                        }
+                        */
         }
 	
 	}
